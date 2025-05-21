fe7b5a5401aafff0fb14cfa5b0c70c71f099161f
Muhammad-Uzair-2611 <muhuzair127@gmail.com>
2025-05-21 10:07:36 +0500

    chore: Remove react-helmet-async and related code

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { StrictMode } from "react";
-import { HelmetProvider } from 'react-helmet-async';
 import "./index.css";
 import App from "./App.jsx";
 import Layout from "./Layout.jsx";
@@ -84,11 +83,9 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <ErrorBoundary>
-      <HelmetProvider>
-        <SearchProvider>
-          <RouterProvider router={routes} />
-        </SearchProvider>
-      </HelmetProvider>
+      <SearchProvider>
+        <RouterProvider router={routes} />
+      </SearchProvider>
     </ErrorBoundary>
   </StrictMode>
 );
